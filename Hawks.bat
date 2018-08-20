2672fd91ae141c90dc09b17f69a3d925acb9269b
JRejouis <42547204+JRejouis@users.noreply.github.com>
2018-08-20 10:28:56 -0500

    Create Hawks.bat
    Run as .bat in same directory as profile copy script

---
+++ Hawks.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+color 02
+echo                     .                                                          
+Timeout /t 1 >nul
+echo                    DOO$.                                                       
+Timeout /t 1 >nul
+echo                   ..8888..                                                     
+Timeout /t 1 >nul
+echo                      D888.                                                     
+Timeout /t 1 >nul
+echo                       Z888$                                                    
+Timeout /t 1 >nul
+echo                       .~888N.                                                  
+Timeout /t 1 >nul
+echo             .  OOZZOO   78888.                              ..                 
+Timeout /t 1 >nul
+echo          ..~ZZ$$$Z$Z$$ZZ.+8888.                           . .O88.              
+Timeout /t 1 >nul
+echo          +ZZZZ$$ZZ$$$$$$Z$ZO888$                           .87~I.              
+Timeout /t 1 >nul
+echo         .DOZZZZZOZZZZ$$$$$Z$O888O.     8ZZZZO8:...          D8OO.              
+Timeout /t 1 >nul
+echo          .OIZ8888$Z$Z$$$$$$$$888$8....DZZZZZZZZZZZZ88...  ..8O8O. ...          
+Timeout /t 1 >nul
+echo              O8~ZD8Z$ZZ$$$ZZ$Z88D,88Z8$N88OZZZZZZZZZZZZD8D$,888O7 :OOZ.        
+Timeout /t 1 >nul
+echo                . .N8$Z$$$ZZ$ZZZ888888$ZZZZZZZZZZZ8ZZZZZ$DDD8O88OZ+OOO8         
+Timeout /t 1 >nul
+echo                    ..?Z$$ZZZZ~8DDDDD888$$ZZZZZZZ88OZZZZZDDD88OOOOOOOO.         
+Timeout /t 1 >nul
+echo                          $$D8DDDDDDDDD88DOZZZZZZZZZZZZZZ7DD888OOOOOZ:.         
+Timeout /t 1 >nul
+echo                            . ,::,:::~$888OOOOZZZZZZZZZZZZ88OOOOOOOO.          
+Timeout /t 1 >nul
+echo                                      .  8888?OZD888OIZZOOZZOO8OOOOZ..          
+Timeout /t 1 >nul
+echo                                          ..8887 ...7$8O8888D88DDD8+            
+Timeout /t 1 >nul
+echo                                              .8O8 .                            
+Timeout /t 1 >nul
+echo                                               . :88Z                           
+Timeout /t 1 >nul
+echo                                                 ..~88$.                       
+Timeout /t 1 >nul
+echo                                                        OOI. .                  
+Timeout /t 1 >nul
+echo                                                          .78..                 
+Timeout /t 1 >nul
+echo                                                             ....               
+Timeout /t 1 >nul
+echo                                                              IT BOYZ
+Timeout /t 2 >nul
+exit
