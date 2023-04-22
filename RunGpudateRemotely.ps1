796764e1e1cfbfda8a1b61c3fc9a7316f6e73088
JRejex <42547204+JRejex@users.noreply.github.com>
2023-04-22 02:49:31 -0500

    Delete RunGpudateRemotely.ps1

--- RunGpudateRemotely.ps1
+++
@@ -1,6 +0,0 @@
-$a = Get-Content "\\INPUT\LOCATION\OF\TEXTFILE\WITH\COMPUTERNAMES\RunGpudateRemotely.txt"
-
-foreach ($i in $a)
-    {$i + "`n" + "=========================="; Invoke-Command -Computername $i -scriptblock {gpupdate /force}
-cmd /c pause}
-
